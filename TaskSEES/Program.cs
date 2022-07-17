51601267042ec5e0b0e863274236eb941c8dcc39
Aliev Qara <alievqara@outlook.com>
2022-07-18 02:38:26 +0400

    task

---
+++ TaskSEES/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace TaskSEES
+{
+    public class Program
+    {
+        public static void Main()
+        {
+
+            User user1 = new User();
+            user1.Name = "Faiq";
+            user1.Surname = "Aliyev";
+            user1.NotRobotCode = "1313";
+            user1.Mounth = 2;
+
+            user1.GetDetail();
+
+
+
+            User user2 = new User();
+            user2.Name = "Tamerlan";
+            user2.Surname = "Aliyev";
+            user2.NotRobotCode = "1413";
+            user2.Mounth = 5;
+
+            user2.GetDetail();
+
+
+
+            User user3 = new User();
+            user3.Name = "Aylin";
+            user3.Surname = "Aliyeva";
+            user3.NotRobotCode = "15513";
+            user3.Mounth = 12;
+
+
+            user3.GetDetail();
+
+
+
+            User user4 = new User();
+            user4.Name = "Teymurleng";
+            user4.Surname = "Aliyev";
+            user4.NotRobotCode = "1613";
+            user4.Mounth = 11;
+
+            user4.GetDetail();
+
+
+
+            User user5 = new User();
+            user5.Name = "Cingiz";
+            user5.Surname = "Xan";
+            user5.NotRobotCode = "173513";
+            user5.Mounth = 1;
+
+            user5.GetDetail();
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
---
+++ TaskSEES/User.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskSEES
+{
+    public class User
+    {
+
+        private static int id { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string NotRobotCode { get; set; }
+
+        public int Mounth { get; set; }
+
+
+        enum RegistrationMonth
+        {
+            January = 1,
+            February = 2,
+            March = 3,
+            April = 4,
+            May = 5,
+            June = 6,
+            July = 7,
+            August = 8,
+            September = 9,
+            October = 10,
+            November = 11,
+            December = 12,
+        }
+
+        public User() => id++;
+
+        public string CheckNotRobotCode(string NotRobotCode)
+        {
+            this.NotRobotCode = NotRobotCode;
+
+            if (NotRobotCode.Length != 4)
+            {
+                return "NotRobotCode Düzgün Göndərilməyib";
+
+            }
+            else
+            {
+                return NotRobotCode;
+            }
+
+        }
+
+
+        public void GetDetail()
+        {
+            Console.WriteLine($"User ID: {id}, Name: {Name}, Surname: {Surname}, NotRobotCode: {CheckNotRobotCode(NotRobotCode)}, Registration Month: {Mounth}");
+
+
+            //switch (Mounth)
+            //{
+            //    case (int)RegistrationMonth.January:
+            //        return;
+            //        break;
+            //    case (int)RegistrationMonth.February:
+            //        return;           
+            //        break;
+            //    case (int)RegistrationMonth.March:
+            //        return;
+            //        break;
+            //    case (int)RegistrationMonth.April:
+            //        return;
+            //        break;
+            //    case (int)RegistrationMonth.May:
+            //        return;
+            //        break;
+            //    case (int)RegistrationMonth.June:
+            //        return;
+            //        break;
+            //    case (int)RegistrationMonth.July:
+            //        return;
+            //        break;
+            //    case (int)RegistrationMonth.August:
+            //        return;
+            //        break;
+            //    case (int)RegistrationMonth.September:
+            //        return;
+            //        break;
+            //    case (int)RegistrationMonth.October:
+            //        return;
+            //        break;
+            //    case (int)RegistrationMonth.November:
+            //        return;
+            //        break;
+            //    case (int)RegistrationMonth.December:
+            //        return;
+            //        break;
+
+            //    default:
+            //        break;
+            //}
+
+
+        }
+
+
+
+
+
+    }
+
+
+}
