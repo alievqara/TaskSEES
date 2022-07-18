615b8e3f213da7a3682401f41bd3b0d024c0ea33
Aliev Qara <alievqara@outlook.com>
2022-07-18 14:44:50 +0400

    task

--- TaskSEES/Program.cs
+++ TaskSEES/Program.cs
@@ -7,58 +7,22 @@ namespace TaskSEES
         public static void Main()
         {
 
-            
-
-            User user1 = new User();
-            user1.Name = "Faiq";
-            user1.Surname = "Aliyev";
-            user1.NotRobotCode = "1313";
-            user1.Mounth = 2;
-
-            user1.GetDetail();
 
 
+            User user1 = new User("Faiq", "Aliyev", "1313", Month.January);
+            Console.WriteLine(user1.GetDetail());
 
+            User user2 = new User("Tamerlan", "Aliyev", "13H53", Month.August);
+            Console.WriteLine(user2.GetDetail());
 
-            User user2 = new User();
-            user2.Name = "Tamerlan";
-            user2.Surname = "Aliyev";
-            user2.NotRobotCode = "1413";
-            user2.Mounth = 5;
+            User user3 = new User("Aylin", "Aliyeva", "13FA", Month.August);
+            Console.WriteLine(user3.GetDetail());
 
-            user2.GetDetail();
+            User user4 = new User("Faiq", "Aliyev", "13JU13", Month.May);
+            Console.WriteLine(user4.GetDetail());
 
-
-
-            User user3 = new User();
-            user3.Name = "Aylin";
-            user3.Surname = "Aliyeva";
-            user3.NotRobotCode = "15513";
-            user3.Mounth = 12;
-
-
-            user3.GetDetail();
-
-
-
-            User user4 = new User();
-            user4.Name = "Teymurleng";
-            user4.Surname = "Aliyev";
-            user4.NotRobotCode = "1613";
-            user4.Mounth = 11;
-
-            user4.GetDetail();
-
-
-
-            User user5 = new User();
-            user5.Name = "Cingiz";
-            user5.Surname = "Xan";
-            user5.NotRobotCode = "173513";
-            user5.Mounth = 1;
-
-            user5.GetDetail();
-
+            User user5 = new User("Faiq", "Aliyev", "1313", Month.January);
+            Console.WriteLine(user5.GetDetail());
 
 
 
--- TaskSEES/User.cs
+++ TaskSEES/User.cs
@@ -16,96 +16,40 @@ namespace TaskSEES
         public string Surname { get; set; }
         public string NotRobotCode { get; set; }
 
-        public int Mounth { get; set; }
+        public Month RegistrationMounth { get; set; }
 
 
-        enum RegistrationMonth
+
+        public User(string name, string surname, string notRobotCode, Month registrationMounth)
         {
-            January = 1,
-            February = 2,
-            March = 3,
-            April = 4,
-            May = 5,
-            June = 6,
-            July = 7,
-            August = 8,
-            September = 9,
-            October = 10,
-            November = 11,
-            December = 12,
-        }
+            Name = name;
+            Surname = surname;
+            NotRobotCode = notRobotCode;
+            RegistrationMounth = registrationMounth;
 
-        public User()
-        {
             id++;
             ID = id;
         }
 
-        public string CheckNotRobotCode(string NotRobotCode)
-        {
-            this.NotRobotCode = NotRobotCode;
+        
 
-            if (NotRobotCode.Length != 4)
+
+        public string GetDetail()
+        {
+            if (NotRobotCode.CheckNotRobotCode())
             {
-                return "NotRobotCode Düzgün Göndərilməyib";
+            return $"User ID: {id}, Name: {Name}, Surname: {Surname}, NotRobotCode: {NotRobotCode}, Registration Month: {RegistrationMounth}";
+
 
             }
             else
             {
-                return NotRobotCode;
+                return $"User ID: {id}, Name: {Name}, Surname: {Surname}, NotRobotCode Düzgün Göndərilməyib , Registration Month: {RegistrationMounth}";
+               
+
             }
 
-        }
-
-
-        public void GetDetail()
-        {
-            Console.WriteLine($"User ID: {id}, Name: {Name}, Surname: {Surname}, NotRobotCode: {CheckNotRobotCode(NotRobotCode)}, Registration Month: {Mounth}");
-
-
-            //switch (Mounth)
-            //{
-            //    case (int)RegistrationMonth.January:
-            //        return;
-            //        break;
-            //    case (int)RegistrationMonth.February:
-            //        return;           
-            //        break;
-            //    case (int)RegistrationMonth.March:
-            //        return;
-            //        break;
-            //    case (int)RegistrationMonth.April:
-            //        return;
-            //        break;
-            //    case (int)RegistrationMonth.May:
-            //        return;
-            //        break;
-            //    case (int)RegistrationMonth.June:
-            //        return;
-            //        break;
-            //    case (int)RegistrationMonth.July:
-            //        return;
-            //        break;
-            //    case (int)RegistrationMonth.August:
-            //        return;
-            //        break;
-            //    case (int)RegistrationMonth.September:
-            //        return;
-            //        break;
-            //    case (int)RegistrationMonth.October:
-            //        return;
-            //        break;
-            //    case (int)RegistrationMonth.November:
-            //        return;
-            //        break;
-            //    case (int)RegistrationMonth.December:
-            //        return;
-            //        break;
-
-            //    default:
-            //        break;
-            //}
-
+          
 
         }
 
@@ -113,6 +57,21 @@ namespace TaskSEES
 
 
 
+    }
+    public enum Month
+    {
+        January = 1,
+        February = 2,
+        March = 3,
+        April = 4,
+        May = 5,
+        June = 6,
+        July = 7,
+        August = 8,
+        September = 9,
+        October = 10,
+        November = 11,
+        December = 12,
     }
 
 
